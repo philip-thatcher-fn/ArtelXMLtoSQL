f702b4883af757c88e4aca6ca8d0ae89b333b3c9
philip-thatcher <53578773+philip-thatcher@users.noreply.github.com>
2019-12-12 23:36:10 -0800

    Commit

--- tutorial.py
+++ tutorial.py
@@ -1,7 +1,16 @@
 # Python code to illustrate parsing of XML files
+import csv
+import requests
+import xml.etree.ElementTree as ET
+
 def loadRSS(url):
     resp = requests.get(url)
     with open('topnewsfeed.xml', 'wb') as f:
         f.write(resp.content)
+
+
+def pareXML(xmlfile):
+
+
 
 loadRSS('http://www.hindustantimes.com/rss/topnews/rssfeed.xml')
