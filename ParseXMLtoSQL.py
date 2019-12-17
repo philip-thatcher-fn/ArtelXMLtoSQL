c67e975c0aea717b9a60a2c3f5ac88deb8b02a5b
philip-thatcher <53578773+philip-thatcher@users.noreply.github.com>
2019-12-16 22:36:08 -0800

    With header info and target volume. Cleaned up code as well.

--- ParseXMLtoSQL.py
+++ ParseXMLtoSQL.py
@@ -1,50 +1,65 @@
 # This script processes Artel Data Manager XML files and pushes relevant data to a MySQL Database
 import xmltodict
 
+
 # Opening file to read with binary encoding and parsing to dict
 def parseXML(path):
     with open(path, 'rb') as f:
         doc = xmltodict.parse(f.read())
     return doc
 
+
 # doc = parseXML('3uL 1_10.xml')
 doc = parseXML('3_10uL_CHi.xml')
 
+# Store header info
+header = doc['Data']['Header3']
+print('Header:')
+print(header)
+
 # Determine file type
-# Create groupsDict with each group
-# Create groupsLst with each group dict in a separate element
-groupsLst = []
+# Create 'groups' (a list) with each group dict as a separate element
+groups = []
 try:
-    # Full Plate Format
-    groupsDict = doc['Data']['Plate']
+    # Full Plate Format Test
+    groupsTmp = doc['Data']['Plate']
 except KeyError:
     # Partial Plate (Group) Format
     print('Parsing in Partial Plate (Group) mode...')
     mode = 'group'
-    groupsDict = doc['Data']['Group']
-    for x in groupsDict:
-        groupsLst.append(x)
+    for x in doc['Data']['Group']:
+        groups.append(x)
 else:
     # Full Plate Format
     print('Parsing in Full Plate mode...')
     mode = 'plate'
-    groupsDict = doc['Data']['Plate']
-    groupsLst.append(groupsDict)
+    groupsTmp = doc['Data']['Plate']
+    groups.append(groupsTmp)
 
 # Examine groupsLst
-groupsLstLen = len(groupsLst)
-print('groupLst Length: ' + str(groupsLstLen))
-print(groupsLst)
+groupsLen = len(groups)
+print('groups Length: ' + str(groupsLen))
+print('Groups:')
+print(groups)
 
-# Loop through groupsLst to pull out info
-for i in range(groupsLstLen):
-    group = groupsLst[i]
+if mode == 'plate':
+    target = doc['Data']['Run_Statistics']['Target_Volume']
+
+# Loop through groupsLst to pull out relevant info
+for i in range(groupsLen):
+    # Write group dict from the group list
+    group = groups[i]
+
+    # get statistics if mode = group
+    if mode == 'group':
+        target = group['Group_Run_Statistics']['Target_Volume']
+
+    # Get volume data
     rows = group['Well_Volumes']['Rows']
     rowsLen = len(rows)
     columnsLen = len(rows[1]['Column'])
     print('Rows: ' + str(rowsLen))
     print('Column: ' + str(columnsLen))
-
 
     values = []  # A1, A2, A3 ... B1 ...
     # Looping through rows
@@ -55,4 +70,7 @@ for i in range(groupsLstLen):
             value = rows[x]['Column'][y]['Value']
             values.append(value)
 
+    print('Target Volume:')
+    print(target)
+    print('Values:')
     print(values)
