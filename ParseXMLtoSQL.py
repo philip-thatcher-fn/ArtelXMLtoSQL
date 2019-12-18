f9e2e3ca95bb4de0a90b37136efffc84121d2a55
philip-thatcher <53578773+philip-thatcher@users.noreply.github.com>
2019-12-18 04:08:36 -0800

    Sql table population working

--- ParseXMLtoSQL.py
+++ ParseXMLtoSQL.py
@@ -1,103 +1,133 @@
 # This script processes Artel Data Manager XML files and pushes relevant data to a MySQL Database
 import xmltodict
+import mysql.connector
 
 
 # Opening file to read with binary encoding and parsing to dict
-def parseXML(path):
+def xmlToDict(path):
     with open(path, 'rb') as f:
         doc = xmltodict.parse(f.read())
     return doc
 
 
-# doc = parseXML('3uL 1_10.xml')
-doc = parseXML('3_10uL_CHi.xml')
-data = doc['Data']
+def getWellData(doc):
+    data = doc['Data']
 
-# Store header info
-header = data['Header3']
-print('Header:')
-print(header)
+    outputData = {}
 
-# Determine file type
-# Create 'groups' (a list) with each group dict as a separate element
-# Try to use case statements or conditionals. Test if plate is present and then if group is present
-groups = None
-if 'Plate' in data:
-    # Full Plate Format
-    print('Parsing in Full Plate mode...')
-    mode = 'plate'
-    # groupsTmp = data['Plate']
-    # groups.append(groupsTmp)
-    groups = [data['Plate']]
-elif 'Group' in data:
-    # Partial Plate (Group) Format
-    print('Parsing in Partial Plate (Group) mode...')
-    mode = 'group'
-    # breakpoint()
-    groups = data['Group']
-else:
-    print('File Format Error')
+    # Create Unique ID
+    sn = data['Plate_Reader']['Serial_Number']
+    fileID = data['FileID']
+    uid = fileID + '_' + sn + '_welldata'
+    outputData['UniqueID'] = uid
 
-# Examine groupsLst
-print(f'groups Length: {len(groups)}')
-print('Groups:')
-print([x['Name'] for x in groups])
-# print([x['Name'] for x in groups if x['Name'] == 'Mom'])
+    # Store header info
+    # header = data['Header3']
+    # print('Header:')
+    # print(header)
 
-# breakpoint()
+    # Determine file type based on presence of 'Plate' key
+    # Create 'groups' (a list) with each group dict as a separate element
+    groups = None
+    if 'Plate' in data:
+        # Full Plate Format
+        print('Parsing in Full Plate mode...')
+        mode = 'plate'
+        groups = [data['Plate']]
+    elif 'Group' in data:
+        # Partial Plate (Group) Format
+        print('Parsing in Partial Plate (Group) mode...')
+        mode = 'group'
+        groups = data['Group']
+    else:
+        print('File Format Error!')
 
-if mode == 'plate':
-    target = data['Run_Statistics']['Target_Volume']
+    # Examine groupsLst
+    # print(f'groups Length: {len(groups)}')
+    # print('Groups:')
+    # # print([x['Name'] for x in groups if x['Name'] == 'Mom'])
 
-# Loop through groupsLst to pull out relevant info
-# for i in range(groupsLen):
-dataOut = {}
-for group in groups:
-    # Write group dict from the group list
-    # group = groups[i]
+    if mode == 'plate':
+        target = data['Run_Statistics']['Target_Volume']
 
-    groupName = group['Name']
+    # Loop through groups to pull out relevant info
+    groupNames = []
+    for group in groups:
+        # remove spaces from group name
+        groupNameTmp = group['Name'].replace(' ', '')
+        groupName = groupNameTmp.lower()
 
-    # get statistics if mode = group
-    if mode == 'group':
-        target = group['Group_Run_Statistics']['Target_Volume']
+        # Push group name to lst
+        groupNames.append(groupName)
 
-    # Get volume data
-    rows = group['Well_Volumes']['Rows']
-    # rowsLen = len(rows)
-    # columnsLen = len(rows[1]['Column'])
-    # print('Rows: ' + str(rowsLen))
-    # print('Column: ' + str(columnsLen))
+        # valuesOut = None  # [[Rows (Alpha)], [Cols (Numeric)], [Values]]
+        # rowsOut = []
+        # colsOut = []
+        # volOut = []
+        groupData = []
 
-    valuesOut = None  # [[Rows (Alpha)], [Cols (Numeric)], [Values]]
-    rowsOut = []
-    colsOut = []
-    volOut = []
-    # Looping through rows
-    # breakpoint()
-    colNames = [x['Name'] for x in group['Well_Volumes']['Columns']['Column']]
-    # row_names = [x['Row'] for x in rows]
-    # print(colNames)
-    for row in rows:
-        # print(rows[x]['Column'])
-        # Looping through 'Column' to get 'Value'
-        # for col in row['Column']:
-        #     values.append(col['Value'])
-        # values.append([x['Value'] for x in row['Column']])
-        # breakpoint()
-        rowName = row['Row']
-        # print(row_name)
-        # breakpoint()
-        # print(rows.index(row))
-        # col_name = col_names[rows.index(row['Column']['Value'])]
-        for (well, colName) in zip(row['Column'], colNames):
-            # print(rowName, colName, well['Value'])
-            rowsOut.append(rowName)
-            colsOut.append(colName)
-            volOut.append(well['Value'])
-    valuesOut = [rowsOut, colsOut, volOut]
-    # breakpoint()
-    print(valuesOut)
-    dataOut[groupName] = valuesOut
+        # get target volume if mode = 'group'
+        if mode == 'group':
+            target = group['Group_Run_Statistics']['Target_Volume']
 
-print(dataOut)
+        rows = group['Well_Volumes']['Rows']
+
+        colNames = [x['Name'] for x in group['Well_Volumes']['Columns']['Column']]
+
+        for row in rows:
+            rowName = row['Row']
+            for (well, colName) in zip(row['Column'], colNames):
+                # rowsOut.append(rowName)
+                # colsOut.append(colName)
+                # volOut.append(well['Value'])
+                groupData.append([rowName, colName, well['Value']])
+
+        # print(groupData)
+
+        outputData[groupName] = groupData
+    outputData['Groups'] = groupNames
+    return outputData
+
+
+def xmlToData(path):
+    # Parsed XML file dict
+    data = xmlToDict(path)
+    # Extract well data
+    wellData = getWellData(data)
+    print(wellData)
+    return wellData
+
+
+def dataToDB(data, dbName):
+    db = mysql.connector.connect(host='localhost',
+                                 user='python_user',
+                                 passwd='*f39SEXJlUG1',
+                                 database=dbName
+                                 )
+    cursor = db.cursor()
+
+    for i in range(len(data['Groups'])):
+        # Create Table
+        tableName = data['UniqueID'] + '_' + data['Groups'][i]
+        col1 = '_row'
+        col2 = '_col'
+        col3 = '_vol'
+        cmd = 'CREATE TABLE ' + tableName + ' (id INT AUTO_INCREMENT PRIMARY KEY, ' + col1 + ' VARCHAR(255), ' + col2 + ' VARCHAR(255), ' + col3 + ' VARCHAR(255))'
+        cursor.execute(cmd)
+        print('Table ' + tableName + ' was created.')
+
+        # Populate Table
+        cmd = 'INSERT INTO ' + tableName + ' (' + col1 + ', ' + col2 + ', ' + col3 + ') VALUES (%s, %s, %s)'
+        currValueKey = data['Groups'][i]
+        values = data[currValueKey]
+        cursor.executemany(cmd, values)
+        db.commit()
+        print(str(cursor.rowcount) + " row(s) were inserted.")
+
+
+pathC96 = '3uL 1_10.xml'
+pathCHi = '3_10uL_CHi.xml'
+
+data = xmlToData(pathC96)
+
+dataToDB(data, 'artel_data_test')
--- sql_test.py
+++ sql_test.py
@@ -14,8 +14,6 @@ db = mysql.connector.connect(host='localhost',
 
 # print(db)
 
-cursor = db.cursor()
-
 # cursor.execute('CREATE DATABASE test_data')
 # cursor.execute('SHOW DATABASES')
 
@@ -33,25 +31,39 @@ cursor = db.cursor()
 # val = ('Mary', 'Chicago')
 # cursor.execute(sql, val)
 
-sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
+# sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+#
+# cursor.executemany(sql, val)
+#
+# db.commit()
+#
+# print(cursor.rowcount, 'Record(s) inserted.')
 
-cursor.executemany(sql, val)
+# cursor.execute('SELECT * FROM customers')
 
-db.commit()
+# cursor.execute('SELECT name, address FROM customers')
 
-print(cursor.rowcount, 'Record(s) inserted.')
+# cursor.execute("SELECT * FROM customers WHERE address LIKE '%way%'")
+#
+# result = cursor.fetchall()
+#
+# for x in result:
+#   print(x)
+
+cursor = db.cursor()
+cursor.execute('CREATE TABLE test_well_data (id INT AUTO_INCREMENT PRIMARY KEY, Group1_Row VARCHAR(255), Group1_Col VARCHAR(255), Group1_Vol VARCHAR(255))')
