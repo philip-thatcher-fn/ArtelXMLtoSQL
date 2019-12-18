6902a4ff8a1ac7d3352d6b1fbfb50e265953f107
philip-thatcher <53578773+philip-thatcher@users.noreply.github.com>
2019-12-17 23:25:10 -0800

    Sql testing

--- sql_test.py
+++ sql_test.py
@@ -1,18 +1,49 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-  host="localhost",
-  user="python_user",
-  passwd="*f39SEXJlUG1"
-)
+db = mysql.connector.connect(host='localhost',
+                             user='python_user',
+                             passwd='*f39SEXJlUG1',
+                             database='test_data'
+                             )
 
 # print(db)
 
 cursor = db.cursor()
 
-# cursor.execute('CREATE DATABASE ARTEL_DATA')
+# cursor.execute('CREATE DATABASE test_data')
+# cursor.execute('SHOW DATABASES')
 
-cursor.execute('SHOW DATABASES')
+# cursor.execute('CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))')
+# cursor.execute('SHOW TABLES')
 
-for x in cursor:
-    print(x)
+# for x in cursor:
+    # print(x)
+
+# cursor.execute('ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY')
+
+# sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
+# val = ('Mary', 'Chicago')
+# cursor.execute(sql, val)
+
+sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+cursor.executemany(sql, val)
+
+db.commit()
+
+print(cursor.rowcount, 'Record(s) inserted.')
